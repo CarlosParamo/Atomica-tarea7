e0352c41318f6f9a3b1ded84508d982b9ec9bc2a
CarlosParamo <capr24398@gmail.com>
2018-04-06 03:07:04 -0500

    second

--- dedito.py
+++ dedito.py
@@ -4,8 +4,10 @@ import matplotlib.pyplot as plt
 n =1500
 du=0.05
 u= np.zeros(n)
-R= np.zeros(n)
-v= np.zeros(n)
+RA= np.zeros(n)
+vA= np.zeros(n)
+RB= np.zeros(n)
+vB= np.zeros(n)
 a= np.zeros(n)
 P= np.zeros(n)
 S= np.zeros(n)
@@ -13,8 +15,10 @@ V= np.zeros(n)
 
 def othersolution(R0,v0,e, W0):
      
-	R[0]=R0
-	v[0]=v0
+	RA[0]=R0
+	vA[0]=v0
+	RB[0]=R0
+	vB[0]=v0
 	u[0]=du
 	P[0]=(u[0]**2)*R[0]**2
 	S[0]=0
@@ -22,13 +26,22 @@ def othersolution(R0,v0,e, W0):
 	for i in range(1,n):
 
 		u[i]=u[i-1]+du
-		v[i]=v[i-1]-((2./u[i-1])*v[i-1]-(e+(2./u[i-1]))*R[i-1])*(du/2.0)
-		R[i]=R[i-1]+v[i]*du
-		P[i]=(u[i]**2)*R[i]**2
+		vA[i]=vA[i-1]-((2./u[i-1])*vA[i-1]-(e-(1./u[i-1]))*R[i-1])*(du/2.0)
+		RA[i]=RA[i-1]+vA[i]*du
+		P[i]=(u[i]**2)*RA[i]**2
 		S[i]=S[i-1]+P[i-1]*du
-                W[i]=W[i-1]+(S[i-1]/u[i-1]
-          
+                
+		#Potencial que sufre B
+		W[i]=W[i-1]+(S[i-1]/u[i-1]**)-2./u[i-1]
 
+		#solucion a schrodinger para B
+		vB[i]=vB[i-1]-((2./u[i-1])*vB[i-1]-(e-W[i]/4.)*R[i-1])*(du/2.0)
+		RB[i]=RB[i-1]+vB[i]*du
+
+
+
+		
+   
 	return R
 
 
